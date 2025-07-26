40b6a40a1f9635bf414583167f52e4571be93145
yassin26-arab <yassindridi0177@gmail.com>
2025-07-26 18:23:38 +0100

    Update js2.js

--- js2.js
+++ js2.js
@@ -1,5 +1,12 @@
+// Select the button element with the class "menu-btn"
 const menuBtn = document.querySelector('.menu-btn');
+
+// Select the navigation element with the class "main-nav"
 const nav = document.querySelector('.main-nav');
+
+// Add a click event listener to the menu button
 menuBtn.addEventListener('click', () => {
+  // Toggle the "show" class on the navigation element
+  // This makes the navigation menu appear or disappear
   nav.classList.toggle('show');
 });
